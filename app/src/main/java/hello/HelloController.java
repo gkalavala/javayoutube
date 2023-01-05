715d6b2f598f6d0f582507ac7140ad3153f283be
gkalavala <115970675+gkalavala@users.noreply.github.com>
2023-01-05 14:53:35 +0530

    update

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot this is si ramesh!";
+        return " this is si ramesh!";
     }   
 }
